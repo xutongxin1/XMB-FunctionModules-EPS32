afd5bc7f2be14593ed2d1e18957e1c1af9e32b01
xiaohuihere <102411813+xiaohuihere@users.noreply.github.com>
2023-01-02 21:03:12 +0800

    tcpserver

---
+++ main.c
@@ -0,0 +1,22 @@
+/* BSD Socket API Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/param.h>
+#include "dap_init.h"
+
+#include "tcp_server1.h"
+
+void app_main() {
+  
+    Dap_Init();
+ 
+}
